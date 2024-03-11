1827744d506ddd983d56c02d7b198691e5ab456a
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-11 08:39:05 -0300

    refactor: change tabWidth

--- react.js
+++ react.js
@@ -25,7 +25,7 @@ module.exports = {
   rules: {
     'prettier/prettier': ["error", {
       'printWidth': 80,
-      'tabWidth': 3,
+      'tabWidth': 2,
       'singleQuote': true,
       'trailingComma': 'all',
       'arrowParens': 'always',
